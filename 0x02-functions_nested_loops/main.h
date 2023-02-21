de2b109369d0eeedd5a4af76c8f466ad74ecba4d
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-02-21 20:02:38 +0000

    Add header file

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+/**
+ *_puchar - print character c to standard output
+ *@c: the character to print
+ *Return: 1 On Success
+ *On error, -1 is returned, error is set appropriately
+ */
+
+int _putchar(char c);
+
+#endif
