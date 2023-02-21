e36a629c22ca5b380083c574a87ddb572dd7877b
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-02-21 20:35:20 +0000

    Add print_alphabet_x10 function

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,9 +11,15 @@
 int _putchar(char c);
 
 /**
- *print_alphabet( - print lowercase alphabet
+ *print_alphabet - print lowercase alphabet
  */
 
 void print_alphabet(void);
+
+/**
+ *print_alphabet_x10 - print lowercase aphabet 10 times
+ */
+
+void print_alphabet_x10(void);
 
 #endif
