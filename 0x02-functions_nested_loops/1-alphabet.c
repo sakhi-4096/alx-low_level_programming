2170b39f4ba02dcdf8268f66853242fcb628483f
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-02-21 20:18:36 +0000

    Print lowercase alphabet function

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_alphabet - print lowercase alphbets
+ */
+
+void print_alphabet(void)
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar('\n');
+}
