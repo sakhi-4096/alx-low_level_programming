566ccf1f421b249092750ccf31e283c3e0e483c5
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-02-23 14:35:05 +0000

    Add print_most_numbers function prototype

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -41,4 +41,11 @@ int mul(int a, int b);
 
 void print_numbers(void);
 
+/**
+ * print_most_numbers - print numbers 0 to 9 except 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void);
+
 #endif /* MAIN_H */
