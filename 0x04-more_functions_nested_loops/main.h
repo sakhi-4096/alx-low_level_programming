dbddfede4597859edb8fa039da79922038b7f4fc
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-02-23 14:28:30 +0000

    Add print_numbers function prototype

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,14 @@
 #define MAIN_H_
 
 /**
+ * _putchar - puts character to standrad output
+ * @c: character to put to standard output
+ * Return: int
+ */
+
+int _putchar(char c);
+
+/**
  * _isupper - check for uppercase character
  * @c: character to be checked
  * Return: 1 if uppercase, 0 if not
