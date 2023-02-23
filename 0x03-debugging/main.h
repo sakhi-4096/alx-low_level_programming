8c01b69245788a3c362ff34abaae1eaa41519eb7
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-02-23 08:37:29 +0000

    Add leap year function prototypes

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -21,4 +21,25 @@ void positive_or_negative(int i);
 
 int largest_number(int a, int b, int c);
 
+/**
+ * convert_day - converts day of month to day of year
+ * accounting for leap year
+ * @month: month in number format
+ * @day: day of month
+ * Return: day of year
+ */
+
+int convert_day(int month, int day);
+
+/**
+ * print_remaining_days - takes a date and prints how many days are left
+ * in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
+
+void print_remaining_days(int month, int day, int year);
+
 #endif /* MAIN_H */
