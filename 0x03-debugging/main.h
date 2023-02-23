0a0f30aab48d76a644f1350648e99c641a8c114c
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-02-23 07:36:47 +0000

    Add positive_or_negative function prototype

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * positive_or_negative - test function for positive or negative
+ * @i: number to be checked
+ * Return: 0
+ */
+
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
