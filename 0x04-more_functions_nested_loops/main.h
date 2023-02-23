888b7a3803b34ed9b4ef3eb0eb4cbfb1a4841945
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-02-23 13:19:25 +0000

    Add main.h header file

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/**
+ * _isupper - check for uppercase character
+ * @c: character to be checked
+ * Return: 1 if uppercase, 0 if not
+ */
+
+int _isupper(int c);
+
+#endif /* MAIN_H */
