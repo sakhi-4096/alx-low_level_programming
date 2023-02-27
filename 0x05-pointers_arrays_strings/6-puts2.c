8624df12babaa484323e2d93a5b3e8229905b22c
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-02-27 22:37:50 +0000

    Print every other character on a string followed by new line

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - skip a char while printing
+ * @str: input string
+ * Return: void
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
