2d10ec1a4a72f53c0ea167263e48a9918d065be1
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-02-27 20:16:16 +0000

    Add _strlen prototype

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -26,4 +26,12 @@ void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
 
+/**
+ * _strlen - return length of a string
+ * @s: string being checked
+ * Return: length
+ */
+
+int _strlen(char *s);
+
 #endif /* MAIN_H */
