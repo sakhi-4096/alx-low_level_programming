2a11d28a617d1e07e20d510e35624927b4fc29ae
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-02-27 19:40:11 +0000

    Add main.h file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/**
+ * _putchar - puts character to standrad output
+ * @c: character to put to standard output
+ * Return: int
+ */
+
+int _putchar(char c);
+
+#endif /* MAIN_H */
