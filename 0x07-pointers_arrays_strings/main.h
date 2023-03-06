8f8380af149eb318ab163dc5307487ca9750efa0
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-03-06 23:57:16 +0000

    Add _strpbrk prototype

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -47,4 +47,13 @@ char *_strchr(char *s, char c);
 
 unsigned int _strspn(char *s, char *accept);
 
+/**
+ * _strpbrk - return pointer to byte in s that matches a byte in accept
+ * @s: string being searched
+ * @accept: target matches
+ * Return: pointer to index of string
+ */
+
+char *_strpbrk(char *s, char *accept);
+
 #endif /* MAIN_H */
