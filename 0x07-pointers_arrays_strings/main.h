6c5e5c610a12e6be063049b08bf4109d13d4dffc
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-03-06 20:21:39 +0000

    Add memset prototype

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,4 +9,14 @@
 
 int _putchar(char c);
 
+/**
+ * _memset - fill memory with a constant byte
+ * @s: memory area pointed to
+ * @b: constant byte
+ * @n: first byted of memory
+ * Return: pointer to memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+
 #endif /* MAIN_H */
