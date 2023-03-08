8d3b396f9dda9bd320a19c5efb6df10dd9debfaa
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-03-08 23:56:43 +0000

    Add _pow_recursion prototype

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -41,4 +41,13 @@ int _strlen_recursion(char *s);
 
 int factorial(int n);
 
+/**
+ * _pow_recursion - return value of x raised to the power of y
+ * @x: base
+ * @y: exponent
+ * Return: x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y);
+
 #endif /* MAIN_H */
