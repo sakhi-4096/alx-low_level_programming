01bbfe07329cca17515b2a97ba777edfb55b4f34
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-03-09 00:29:45 +0000

    Add _sqrt_recursion prototype

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -50,4 +50,12 @@ int factorial(int n);
 
 int _pow_recursion(int x, int y);
 
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: given number
+ * Return: natural square root
+ */
+
+int _sqrt_recursion(int n);
+
 #endif /* MAIN_H */
