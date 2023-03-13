bf476184e616e6e5068ec6e64cf24f67126398d4
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-03-13 22:04:18 +0000

    Return a pointer to a duplicate string given as a parameter

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - get length of string
+ * @s: input string
+ * Return: length of string in bytes
+ */
+
+size_t _strlen(char *s)
+{
+	size_t i, len = 0;
+
+	for (i = 0; *(s + i) != '\0'; i++, len++)
+		;
+	return (len);
+}
+
+/**
  * _strdup - return pointer to a duplicate string given as parameter
  * @str: string given as parameter
  * Return: pointer to copy of string
@@ -9,18 +24,19 @@
 
 char *_strdup(char *str)
 {
-	size_t i;
+	size_t i, length;
 	char *new_str;
 
 	if (str == NULL)
 		return (NULL);
 
-	new_str = (char *) malloc(sizeof(*str));
+	length = _strlen(str);
+	new_str = (char *) malloc(length);
 	if (new_str == NULL)
 		return (NULL);
 
 	i = 0;
-	while (*(str + i) != '\0')
+	while (i < length)
 	{
 		*(new_str + i) = *(str + i);
 		i++;
