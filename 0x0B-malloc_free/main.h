48d6f4cf09d28e27b499b8a993c63ca68eb95d07
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-03-14 23:16:54 +0000

    Add free_grid prototype

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -44,4 +44,13 @@ char *str_concat(char *s1, char *s2);
 
 int **alloc_grid(int width, int height);
 
+/**
+ * free_grid - frees a 2 dimensional grid
+ * @grid: 2 dimensional array
+ * @height: vertical height
+ * Return: void
+ */
+
+void free_grid(int **grid, int height);
+
 #endif /* MAIN_H */
