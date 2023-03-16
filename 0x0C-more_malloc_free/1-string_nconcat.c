b17f4e88567c36314fb5b8681d33b896a46e3b24
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-03-16 21:01:35 +0000

    Concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,22 +11,39 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	size_t i, j, len = 0;
+	size_t i = 0, j = 0, len1 = 0, len2 = 0;
 	char *s_concat;
 
-	/* handle NULL strings */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	/* get length of s2 */
-	while (*(s2 + len) != '\0')
-		len++;
-	len++, len += n; /* +1 handle NULL pointer */
+	/* length of strings */
+	while (s1 && *(s1 + len1))
+		len1++;
+	while (s2 && *(s2 + len2))
+		len2++;
 
-	s_concat = malloc(sizeof(*s_concat) * len);
-	if (s_concat == NULL)
+	/* allocate memory for string */
+	if (n < len2)
+		s_concat = malloc(sizeof(*s_concat) * (len1 + n + 1));
+	else
+		s_concat = malloc(sizeof(*s_concat) * (len1 + len2 + 1));
+	if (s_concat == NULL) /* check validity of memory block */
 		return (NULL);
+	
+	/*while (i < len1)
+	{
+		s[i] = s1[i];
+		i++;
+	}
+
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+	s[i] = '\0';*/
 
 	for (i = 0; *(s1 + i) != '\0'; i++)
 		*(s_concat + i) = *(s1 + i);
@@ -35,4 +52,5 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	*(s_concat + (i + j)) = '\0';
 
 	return (s_concat);
+
 }
