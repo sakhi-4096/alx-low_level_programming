8aec956e64a8e1f247ed810f8d01daa41f1bac49
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-03-16 19:29:50 +0000

    Add string_nconcat prototype

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -17,4 +17,14 @@ int _putchar(char c);
 
 void *malloc_checked(unsigned int b);
 
+/**
+ * string_nconcat - concatenate two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: character used from second string
+ * Return: pointer to new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+
 #endif /* MAIN_H */
