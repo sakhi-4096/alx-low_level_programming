37d99945df08b0af72659108708c0459c5d111e7
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-03-17 11:16:28 +0000

    Add _realloc prototype

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -45,4 +45,14 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 
 int *array_range(int min, int max);
 
+/**
+ * _realloc - reallocates memory block using malloc and free
+ * @ptr: pointer to previously allocated memory
+ * @old_size: size, in bytes allocated space for ptr
+ * @new_size: size, in bytes of new memory block
+ * Return: pointer to new memory block
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
 #endif /* MAIN_H */
