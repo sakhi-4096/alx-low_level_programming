1bb34ce7155cef8c1ca31d98caefd0e13fae38e2
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-03-30 18:22:01 +0000

    Print the elements of a list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - print all elements of a list
+ * @h: list to be printed
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
