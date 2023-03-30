d1ee61f5a2542e48fcccbcb08c8fbeccd3b0bc5f
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-03-30 19:05:28 +0000

    Add stdlib.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,6 +2,8 @@
 #define LISTS_H_
 
 #include <stddef.h>
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * struct list_s - singly linked list
@@ -31,5 +33,13 @@ size_t print_list(const list_t *h);
  * Return: number of elements
  */
 size_t list_len(const list_t *h);
+
+/**
+ * add_node - add a new node at the beginning of a list
+ * @head: pointer to head of linked list
+ * @str: name being added
+ * Return: address of new element
+ */
+list_t *add_node(list_t **head, const char *str);
 
 #endif /* LISTS.H */
