b647aafc9a06599608fc06fd994565191b121509
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-04-04 13:44:02 +0000

    Add lists.h header file

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,12 @@
+#ifndef LISTS_H_
+#define LISTS_H_
+
+/**
+ * _putchar - puts character to standrad output
+ * @c: character to put to standard output
+ * Return: int
+ */
+
+int _putchar(char c);
+
+#endif /* LISTS.H */
