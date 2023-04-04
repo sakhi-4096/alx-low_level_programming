82711d85d4e28919f04cb632119a084691256c20
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-04-04 18:05:14 +0000

    Add print_listint prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
 #ifndef LISTS_H_
 #define LISTS_H_
 
+#include <stdio.h>
+#include <stddef.h>
+
 /**
  * _putchar - puts character to standrad output
  * @c: character to put to standard output
@@ -8,5 +11,25 @@
  */
 
 int _putchar(char c);
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/**
+ * print_listint - print elements of linked list
+ * @h: pointer to head link
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h);
 
 #endif /* LISTS.H */
