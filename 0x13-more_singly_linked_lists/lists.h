4f0807004b181aec8560def23fe29b2630ec1329
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-04-05 01:26:06 +0000

    Add insert_nodeint_at_index prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -92,4 +92,13 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
  */
 int sum_listint(listint_t *head);
 
+/**
+ * insert_nodeint_at_index - insert node at given position
+ * @head: linked list head
+ * @idx: index for insertion
+ * @n: data being insterted
+ * Return: address of new node, NULL if failed
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+
 #endif /* LISTS.H */
