aa42b546efde6e77119a5aa007977e4bd5bd8833
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-04-06 03:37:07 +0000

    Add print_listint_safe prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -116,4 +116,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index);
  */
 listint_t *reverse_listint(listint_t **head);
 
+/**
+ * print_listint_safe - print a listint_t linked list
+ * @head: pointer to linked list
+ * Return: number of nodes, 98 if fail
+ */
+size_t print_listint_safe(const listint_t *head);
+
 #endif /* LISTS.H */
