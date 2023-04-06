77f83323174bf120b3b8d0275df5f2a29558a4a5
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-04-06 02:49:36 +0000

    Add reverse_listint protootype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -109,4 +109,11 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 
+/**
+ * reverse_listint - reverse a listint linked list
+ * @head: linked list
+ * Return: pointer to first node of reverserd list
+ */
+listint_t *reverse_listint(listint_t **head);
+
 #endif /* LISTS.H */
