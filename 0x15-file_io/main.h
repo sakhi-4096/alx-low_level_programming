2356879df9587a50c043ce967a6e35e3e364510f
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-04-10 13:41:15 +0000

    Add main.h header file

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <unistd.h>
+#include <stdio.h>
+
+/**
+ * _putchar - puts character to standrad output
+ * @c: character to put to standard output
+ * Return: int
+ */
+int _putchar(char c);
+
+#endif /* MAIN_H */
