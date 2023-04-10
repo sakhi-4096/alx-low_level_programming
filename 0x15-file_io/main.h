3a8551317055b6c5ad0047a674854f59f721446b
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-04-10 18:58:41 +0000

    Add create_file prototype

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -24,4 +24,12 @@ int _putchar(char c);
  */
 ssize_t read_textfile(const char *filename, size_t letters);
 
+/**
+ * create_file - create a file.
+ * @filename: name of file to create
+ * @text_content: string to be written to file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content);
+
 #endif /* MAIN_H */
