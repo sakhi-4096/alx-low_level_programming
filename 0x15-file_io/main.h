4bc8d57588bd6561190d19f6b253e9cbdeeffae3
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-04-10 20:25:17 +0000

    Add append_text_to_file prototype

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -32,4 +32,12 @@ ssize_t read_textfile(const char *filename, size_t letters);
  */
 int create_file(const char *filename, char *text_content);
 
+/**
+ * append_text_to_file - append text to at the end of a file
+ * @filename: name of the file
+ * @text_content: string to be added
+ * Return: 1 on success, 0 on failure
+ */
+int append_text_to_file(const char *filename, char *text_content);
+
 #endif /* MAIN_H */
