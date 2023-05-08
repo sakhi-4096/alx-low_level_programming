0f05c790e4d8b4c1cd4feb4d32141d5e57b6ee4f
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-05-08 15:30:23 +0000

    Create a new file with specific permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,23 +28,21 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	/* create file with permissions and check if successful */
-	o = open(filename, O_CREAT | O_EXCL | O_RDWR, 0600);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (o == -1)
 		return (-1);
 
-	if (text_content == NULL)
+	if (text_content != NULL) /* write content to file */
 	{
-		close(o);
-		return (1);
+		str_len = _strlen(text_content);
+		w = write(o, text_content, str_len);
+		if (w == -1 || w != str_len) /* failure to write, and return */
+		{
+			close(o);
+			return (-1);
+		}
 	}
-	str_len = _strlen(text_content);
 
-	w = write(o, text_content, str_len); /* write content to file */
-	if (w == -1 || w != str_len) /* failure to write, and return */
-	{
-		close(o);
-		return (-1);
-	}
 	close(o); /* close file */
 	return (1);
 }
