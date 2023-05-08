8547a34af9a7cb72670c722ef187988e8783d4ff
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-05-08 15:17:15 +0000

    Create a new file with specific permissions

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,6 +8,7 @@
 size_t _strlen(char *s)
 {
 	size_t count = 0;
+
 	while (*(s + count) != '\0')
 		count++;
 	return (count);
@@ -22,26 +23,25 @@ size_t _strlen(char *s)
 int create_file(const char *filename, char *text_content)
 {
 	int o, w, str_len = 0;
-	
+
 	if (filename == NULL)
 		return (-1);
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600); /* create file with permisions */
+	/* create file with permissions and check if successful */
+	o = open(filename, O_CREAT | O_EXCL | O_RDWR, 0600);
 	if (o == -1)
 		return (-1);
 
-	if (text_content == NULL) /* nothing to write, return created file */
+	if (text_content != NULL) /* write content to file */
 	{
-		close(o);
-		return (1);
-	}
-	str_len = _strlen(text_content); /* get length of chars to write */
-
-	w = write(o, text_content, str_len);
-	if (w == -1 || w != str_len) /* failure to write, return */
-	{
-		close(o);
-		return (-1);
+		str_len = _strlen(text_content);
+		w = write(o, text_content, str_len);
+		if (w == -1 || w != str_len) /* failure to write, delete file and return */
+		{
+			close(o);
+			unlink(filename);
+			return (-1);
+		}
 	}
 
 	close(o); /* close file */
