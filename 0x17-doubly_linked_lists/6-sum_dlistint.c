18e0968dd7ec4912c34b8ceefa54d035597a81dc
Sakhile Ndlazi <sakhilelindah@protonmail.com>
2023-06-15 22:32:09 +0000

    Rafactor code according to coding guidelines.

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -3,7 +3,7 @@
 /**
  * sum_dlistint - return the sum of data of a linked list
  * @head: pointer to the head
- * @Return: sum of data, 0 if empty
+ * Return: sum, 0 if empty
  */
 int sum_dlistint(dlistint_t *head)
 {
