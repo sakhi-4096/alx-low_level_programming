33ba28865c91fecc182c90ee3841906db715764a
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-10-11 01:45:47 +0200

    Add header file.

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+
+#include <stdio.h>
+#include <stddef.h>
+
+/* Function prototypes */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif /* search_algos.h */
