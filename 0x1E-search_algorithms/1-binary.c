911cdcaffcb529eb1bcc4aa9365192efbafb9b3f
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-10-12 01:12:43 +0200

    Fix style guideline.

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,31 +10,30 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-  int left = 0;
-  int right = size - 1;
+	int left = 0;
+	int right = size - 1;
 
-  if (array == NULL)
-    return (-1);
+	if (array == NULL)
+		return (-1);
 
-  while (left <= right)
-  {
-    int mid = left + (right - left) / 2;
+	while (left <= right)
+	{
+		int i, mid = left + (right - left) / 2;
 
-    printf("Searching in array: %d", array[left]);
-    for (int i = left + 1; i <= right; i++)
-    {
-      printf(", %d", array[i]);
-    }
-    printf("\n");
+		printf("Searching in array: %d", array[left]);
+		for (i = left + 1; i <= right; i++)
+		{
+			printf(", %d", array[i]);
+		}
+		printf("\n");
 
-    if (array[mid] == value)
-      return (mid);
+		if (array[mid] == value)
+			return (mid);
 
-    if (array[mid] < value)
-      left = mid + 1;
-    else
-      right = mid - 1;
-  }
-
-  return (-1); /* Value not found */
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1); /* Value not found */
 }
