07728d08a044aa0d44f4e73ec21e2c8bc1b7df3e
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-10-12 03:45:33 +0200

    Perform advanced binary search on sorted array of integers.

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -11,33 +11,35 @@
  */
 int advanced_binary_recursive(int *array, int low, int high, int value)
 {
-  if (low <= high)
-  {
-    printf("Searching in array: %d", array[low]);
-    for (int i = low + 1; i <= high; i++)
-    {
-      printf(", %d", array[i]);
-    }
-    printf("\n");
+	int i, mid;
 
-    int mid = low + (high - low) / 2;
+	if (low <= high)
+	{
+		printf("Searching in array: %d", array[low]);
+		for (i = low + 1; i <= high; i++)
+		{
+			printf(", %d", array[i]);
+		}
+		printf("\n");
 
-    if (array[mid] == value)
-    {
-      if (array[mid - 1] == value)
-        return (advanced_binary_recursive(array, low, mid, value));
-      return (mid);
-    }
-    if (array[mid] < value)
-      return (advanced_binary_recursive(array, mid + 1, high, value));
-    return (advanced_binary_recursive(array, low, mid - 1, value));
-  }
+		mid = low + (high - low) / 2;
 
-  return (-1); /* Value not found */
+		if (array[mid] == value)
+		{
+			if (array[mid - 1] == value)
+				return (advanced_binary_recursive(array, low, mid, value));
+			return (mid);
+		}
+		if (array[mid] < value)
+			return (advanced_binary_recursive(array, mid + 1, high, value));
+		return (advanced_binary_recursive(array, low, mid - 1, value));
+	}
+
+	return (-1); /* Value not found */
 }
 
 /**
- * advanced_binary - Perform advanced binary search on a sorted array of integers.
+ * advanced_binary - Perform advanced binary search on sorted array of integers
  * @array: Pointer to the first element of the sorted array to search in.
  * @size: Number of elements in the array.
  * @value: The value to search for.
@@ -46,8 +48,8 @@ int advanced_binary_recursive(int *array, int low, int high, int value)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-  if (array == NULL)
-    return (-1);
+	if (array == NULL)
+		return (-1);
 
-  return (advanced_binary_recursive(array, 0, size - 1, value));
+	return (advanced_binary_recursive(array, 0, size - 1, value));
 }
